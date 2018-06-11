045f763e60a01f457ed0d152b58db21aac04b2ae
veryspry <ehlinger.matt@gmail.com>
2018-06-11 11:35:28 -0500

    spy not working

--- bubblesort.js
+++ bubblesort.js
@@ -1,22 +1,28 @@
 function bubbleSort(arr) {
-  let temp;
+
   let limit = arr.length;
 
   while (limit--) {
     for (let i = 0, j = 1; i < arr.length; i++, j++) {
       if (arr[i] > arr[j]) {
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
+        arr = swap(arr, i, j);
       }
     }
   }
-
   return arr;
 }
 
-let arr = [9, 8, 3, 2];
-console.log(bubbleSort(arr));
+
+function swap(arr, i, j) {
+  let temp;
+  temp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = temp;
+  return arr;
+}
+
+// let arr = [9, 8, 3, 2];
+// console.log(bubbleSort(arr));
 
 // Look at the first item
 // Compare to second item
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,13 +1,24 @@
 describe("Bubble Sort", function() {
+  let arr;
+  beforeEach(() => {
+    arr = [2, 4, 6, 3];
+  });
+
   it("handles an empty array", function() {
     expect(bubbleSort([])).toEqual([]);
   });
-
-  beforeEach(() => {
-    let arr = [2, 4, 6, 3];
-  });
+// let arr = [2, 4, 6, 3];
 
   it("sorts an array", function() {
     expect(bubbleSort(arr)).toEqual([2, 3, 4, 6]);
+  });
+
+
+  // it('getting to the center of tootsiepop involves exactly three licks', function () {
+  //   expect(swap.count()).toEqual(2);
+  // });
+
+  it("tracks that the spy was called x times", function() {
+    expect(swap).toHaveBeenCalledTimes(2);
   });
 });
